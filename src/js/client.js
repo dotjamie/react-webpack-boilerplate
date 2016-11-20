4659f5bbe95a9e6da2bf5ee187267d3c05e35104
dotjamie <dotjamie@gmail.com>
2016-11-19 21:26:42 -0800

    initial commit

---
+++ src/js/client.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hiya</h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Main />,
+  document.getElementById('app')
+)
